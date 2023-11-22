27e4446751ce5214b0d94ed5f9099407800835a0
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-11-22 22:19:23 +0100

    Added search to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,40 @@ import { FiBell } from "react-icons/fi";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { FiPlus } from "react-icons/fi";
 import profilepic from "../img/profilepic.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Menu from "./Menu";
 
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+  const handleSearchInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    // Redirect to the profile page with the entered username
+    // You can use React Router or any other method you prefer for navigation
+    // For simplicity, let's assume the profile page URL is '/profile/:username'
+    navigate(`/profile/${searchInput}`);
+  };
   return (
     <nav className="navbar">
       {/* left */}
       <div className="navLeft">
-       <img src={profilepic} alt="" />
+        <Link to='/'> <img src={img1} alt="" /></Link>
+
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Search or jump to"
+            value={searchInput}
+            onChange={handleSearchInputChange}
+          />
+        </form>
       </div>
 
       {/* center */}
@@ -30,11 +55,12 @@ function Navbar() {
           <FiPlus />
           <IoMdArrowDropdown />
         </div>
-        <div className="navRightIcons">
+        <div className="navRightIcons" onClick={() => setShowMenu(!showMenu)}>
           <img src={profilepic} alt="" />
           <IoMdArrowDropdown />
         </div>
       </div>
+      {showMenu && <Menu />}
     </nav>
   );
 
