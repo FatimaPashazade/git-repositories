5349d5f37e9fae4108661dc96507b0535a2e0d90
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-11-22 22:33:00 +0100

    Added new routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.scss';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="App">
-     <Navbar/>
-     <Routes>
-       <Route path="/" element={<Main/>}/>
-     </Routes>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/profile" />} />
+          <Route path="/profile/:searchInput" element={<Profile />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </BrowserRouter>
- );
+  );
 }
 
 export default App;
