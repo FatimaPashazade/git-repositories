5601cce4f8ebeaadc89ed2a284f8ec6bd9c8e219
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-11-22 17:46:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Navbar from './components/Navbar';
+
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+
+    <div className="App">
+     <Navbar/>
+    </div>
+
+ );
+}
+
+export default App;
